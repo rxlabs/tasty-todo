4ec10c4134628fea2adb2142d90bc0b4c128e01b
Evan Sosenko <razorx@evansosenko.com>
2016-03-30 11:56:47 -0700

    Add Provider, store, and single reducer

--- app/index.js
+++ app/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
+import reducer from './reducers'
 import App from './components/App'
+
+const store = createStore(reducer)
 
 export default () => {
   injectTapEventPlugin()
   render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('root')
   )
 }
