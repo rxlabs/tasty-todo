dec9cff2cf2dbf748dda0248a49b2a9438148d25
Evan Sosenko <razorx@evansosenko.com>
2016-03-30 11:35:46 -0700

    Simplify index.js

--- app/index.js
+++ app/index.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import App from './components/App'
 
-const rootEl = document.getElementById('root')
-
-function render () {
-  ReactDOM.render(
-    <App />,
-    rootEl
-  )
-}
-
 export default () => {
   injectTapEventPlugin()
-  render()
+  render(
+    <App />,
+    document.getElementById('root')
+  )
 }
